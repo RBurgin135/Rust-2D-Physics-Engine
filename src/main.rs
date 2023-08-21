493ce3bfbb5236d8753f54fa7039bf5a526cb618
RBurgin <lonelymanwillspamyou@gmail.com>
2023-08-21 23:49:53 +0100

    crate setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        draw_circle(screen_width() /2.0, screen_height() /2.0, 15.0, YELLOW);
+
+        //define floor
+        let thickness = 3.0;
+        let vertical_buffer = 50.0;
+        draw_line(0.0, screen_height()-vertical_buffer, screen_width(), screen_height()-vertical_buffer, thickness, BLUE);
+
+        next_frame().await
+    }
 }
